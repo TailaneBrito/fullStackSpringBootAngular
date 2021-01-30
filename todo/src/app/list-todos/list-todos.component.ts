a39a75d1a21cbb3a2c8e19dc0eea1a09a0353f39
TailaneBrito <tailane.brito01@gmail.com>
2021-01-30 14:35:08 -0300

    Todo Class and using pipe to style the Date

--- todo/src/app/list-todos/list-todos.component.ts
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id : number,
+    public description : string,
+    public done : boolean,
+    public targetDate : Date
+  ){
+
+  }
+
+}
+
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,11 +22,17 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id : 1, description : 'Learning to Dance' },
-    { id : 2, description : 'Learning to Eat' },
-    { id : 3, description : 'Learning to Walk' },
-    { id : 4, description : 'Learning to Code' },
-    { id : 5, description : 'Learning to Say No' },
+    new Todo(1,'Learning to Dance', false, new Date()),
+    new Todo(2,'Learning to Eat', false, new Date()),
+    new Todo(3,'Learning to Walk', true, new Date()),
+    new Todo(4,'Learning to Code', false, new Date()),
+    new Todo(5,'Learning to Say No', false, new Date())
+
+    // { id : 1, description :  },
+    // { id : 2, description :  },
+    // { id : 3, description :  },
+    // { id : 4, description :  },
+    // { id : 5, description :  },
   ]
 
   // todo = {
